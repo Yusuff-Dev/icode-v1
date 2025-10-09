0192a73b119d8ec3695e74ac9d9efb029d2ce2a2
Yusuf <abrorov.muxriddin@gmail.com>
2025-10-10 00:13:02 +0500

    blog section has done

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,55 +1,84 @@
 import { useEffect, useState } from "react";
 import SectionTitle from "../UI/SectionTitle";
-import blogs1 from '../assets/blogs1.png'
+import blogs1 from "../assets/blogs1.png";
+import blogs2 from "../assets/blogs2.png";
+import blogs3 from "../assets/blogs3.png";
 
 interface BlogsInterface {
-  id: number | string,
-  date: string,
-  img: string,
-  desc: string
+  id: number | string;
+  date: string;
+  img: string;
+  desc: string;
 }
 
-const blogsData:BlogsInterface[] = [
+const blogsData: BlogsInterface[] = [
   {
     id: 1,
-    date: '20 June/2020',
+    date: "20 June/2020",
     img: blogs1,
-    desc: 'Technology is evolving rapidly. New devices and applications.'
-  }
-]
+    desc: "Technology is evolving rapidly. New devices and applications.",
+  },
+
+  {
+    id: 2,
+    date: "20 June/2020",
+    img: blogs2,
+    desc: "New devices and applications come to the market.",
+  },
+
+  {
+    id: 3,
+    date: "20 June/2020",
+    img: blogs3,
+    desc: "Every day providing solutions to problems.",
+  },
+];
 
 export default function Blog() {
   const [blogs, setBlogs] = useState<BlogsInterface[]>([]);
 
-  useEffect(()=>{
-    setBlogs(blogsData)
-  }, [])
+  useEffect(() => {
+    setBlogs(blogsData);
+  }, []);
 
-  useEffect(() => { }, [])
+  useEffect(() => {}, []);
   return (
     <section className="mt-25">
       <div className="container">
-        <div className="flex gap-[25px]">
+        <div className="flex gap-[25px] flex-wrap">
+
           <div className="max-w-[165px]">
             <SectionTitle heading="Get News Feeds">Blog</SectionTitle>
           </div>
-          <div className="flex ">
-            {
-              blogs?.map(blog=>(
-                <div key={blog.id} className="group w-[255px] h-[350px] cursor-pointer bg-white shadow hover:w-[350px] transition-all duration-500 origin-bottom">
-                  <div className="w-full h-full group-hover:h-[233px] transition-all duration-200 object-cover">
-                    <img src={blog.img} alt={blog.desc} className="w-full h-full object-cover"/>
+
+          <div className="flex grow gap-10">
+            {blogs.length ? (
+              blogs.map((blog) => (
+                <div key={blog.id} className="group grow-1 cursor-pointer hover:grow-2 duration-200 h-[350px] relative">
+                  <div className="w-full h-full absolute group-hover:h-[233px] duration-200">
+                    <img
+                      src={blog.img}
+                      alt={blog.desc}
+                      className="w-full h-full object-cover"
+                    />
                   </div>
-                  {/* <div className="text-white absolute w-full h-full flex flex-col justify-end gap-[10px] bg-[url(./card-bg.png)] bg-cover group-hover:bg-white">
-                    <p className="group-hover:text-purple-500 font-medium">{blog.date}</p>
-                    <h4 className="group-hover:text-black text-xl font-bold leading-[100%]">{blog.desc}</h4>
-                  </div> */}
+                  <div className="absolute w-full h-full bg-[url(./card-bg.png)] group-hover:opacity-0 duration-200"></div>
+                  <div className="absolute w-full h-full flex flex-col justify-end gap-[10px] py-[30px] px-5 group-hover:p-0 duration-200">
+                    <p className="text-white group-hover:text-purple-500 duration-200">
+                      {blog.date}
+                    </p>
+                    <h4 className="text-white group-hover:text-[#000] font-bold text-xl duration-200">
+                      {blog.desc}
+                    </h4>
+                  </div>
                 </div>
               ))
-            }
+            ) : (
+              <h4 className="text-xl text-black text-center">There is no data!</h4>
+            )}
           </div>
         </div>
       </div>
     </section>
-  )
+  );
 }
